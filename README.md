c1fd58e9bacd6f30ac7f1535eba01ad72bd57fa9
penghaige <2316801207@qq.com>
2018-07-11 16:07:14 +0800

    test

---
+++ README.md
@@ -0,0 +1,9 @@
+
+模块名：Mongo1
+项目名称：testMongoDBInterface
+包名：com.pagoda
+版本号：1.0.0
+git地址：https://github.com/penghaige/data.git
+
+install.bat 将各模块编译成jar包后复制到本地的maven仓库
+deploy.bat 将各模块编译成jar包后上传到公司私有的maven仓库
---
+++ mongo1-api/src/main/java/com/pagoda/api/dto/group/BasTestGoodsDataDTO.java
@@ -0,0 +1,80 @@
+package com.pagoda.api.dto.group;
+
+import com.pagoda.api.dto.*;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.pagoda.platform.jms.annotation.*;
+import lombok.Data;
+import lombok.experimental.Accessors;
+import java.lang.reflect.*;
+import java.io.Serializable;
+import java.util.function.*;
+import java.util.*;
+import io.swagger.annotations.*;
+import org.springframework.data.domain.*;
+import org.springframework.validation.*;
+
+import static com.pagoda.api.dto.ValidatorBuilder.Predicates.*;
+import static com.pagoda.api.dto.group.BasTestGoodsDataDTO.Getters.*;
+
+/**
+ * 查询方法返回的DTO对象
+ *
+ * @author PagodaGenerator
+ * @generated
+ */
+@Data
+@Accessors(chain = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@ValidateProperties(rulePrefix = "validator.BasTestGoodsData")
+@ApiModel(value = "BasTestGoodsDataDTO", description = "商品定义")
+@EntityFeature(
+  entityName = "BasTestGoodsDataDTO",
+  idField = "id",
+  persistent = true,
+  generationType = "auto",
+  batchImport = false,
+  treeStyle = false,
+  auditable = false,
+  traceable = false,
+  approvalRequired = false,
+  requestUrl = "",
+  tableMultiSelect = false
+)
+public class BasTestGoodsDataDTO extends AbstractDTO implements Serializable {
+
+  @FieldMeta(
+    name = "id",
+    nameCN = "主键id",
+    type = "long",
+    visible = false,
+    canQuery = false,
+    readOnly = true
+  )
+  @ApiModelProperty(name = "id", value = "主键id", dataType = "long", notes = "")
+  private Long id;
+
+  @FieldMeta(
+    name = "deleted",
+    nameCN = "软删除标记",
+    type = "long",
+    visible = false,
+    canQuery = false,
+    readOnly = true
+  )
+  @ApiModelProperty(name = "deleted", value = "软删除标记", dataType = "long", notes = "")
+  private Long deleted;
+
+  /** DTO字段的getter */
+  public static class Getters {
+    public static final Function<BasTestGoodsDataDTO, Long> _id = o -> o.getId();
+    public static final Function<BasTestGoodsDataDTO, Long> _deleted = o -> o.getDeleted();
+  }
+
+  public static ValidatorBuilder<BasTestGoodsDataDTO> validatorExample() {
+    ValidatorBuilder<BasTestGoodsDataDTO> builder = new ValidatorBuilder<>();
+    return builder;
+  }
+
+  /** 存储页面post请求的分页参数 */
+  private Pageable pageable;
+}
