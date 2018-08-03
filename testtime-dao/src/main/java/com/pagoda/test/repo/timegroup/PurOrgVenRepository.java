0d6cf52cf77a7ef8b3fb8458609e88c05469ad4b
penghaige <2316801207@qq.com>
2018-08-03 19:02:42 +0800

    test Time

---
+++ testtime-dao/src/main/java/com/pagoda/test/repo/timegroup/PurOrgVenRepository.java
@@ -0,0 +1,29 @@
+package com.pagoda.test.repo.timegroup;
+
+import com.pagoda.platform.jms.annotation.SqlTemplate;
+import com.pagoda.platform.jms.jpa.*;
+import com.pagoda.test.api.dto.timegroup.*;
+import com.pagoda.test.domain.timegroup.*;
+import com.pagoda.test.repo.timegroup.custom.*;
+
+import java.util.*;
+import java.util.concurrent.*;
+
+import org.mybatis.dynamic.sql.select.render.SelectStatementProvider;
+import org.mybatis.dynamic.sql.update.render.UpdateStatementProvider;
+import org.springframework.data.domain.*;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.*;
+import org.springframework.data.repository.query.*;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * PurOrgVen 数据访问接口
+ *
+ * @author PagodaGenerator
+ * @generated
+ */
+public interface PurOrgVenRepository
+    extends BasePurOrgVenRepository,
+        PurOrgVenRepositoryCustom,
+        PagodaJpaRepository<PurOrgVen, Long> {}
